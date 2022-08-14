7a5fcabff8fcf3d889f84aadc169beeee6b6f004
Dalia-Alawneh <alawnehdalia340@gmail.com>
2022-08-14 16:32:59 +0300

    home edit

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,6 +3,7 @@ import 'style.dart';
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
   final Style style = Style();
+
   @override
   Widget build(BuildContext context) {
     return  SafeArea(
