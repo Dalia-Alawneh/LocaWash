c5bec244a27a944a9bddc50fdd7a88e56f36f6a2
Dalia-Alawneh <alawnehdalia340@gmail.com>
2022-08-16 09:38:36 +0300

    final edit

--- lib/style.dart
+++ lib/style.dart
@@ -1,6 +1,7 @@
 class Style{
   int primaryBlue = 0xff141C43;
   int primaryPink = 0xffF54168;
+  int secondaryPink = 0xffF54168CE;
   int gradientTop = 0xffD2D4E1;
   int gradientCenter = 0xff777B96;
   int gradientBottom = 0xff3B4165;
