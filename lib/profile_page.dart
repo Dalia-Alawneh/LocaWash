05021cb6abfceb672980ccfe9af9983a0d5810f6
Dalia-Alawneh <alawnehdalia340@gmail.com>
2022-08-22 23:20:15 +0300

    profile edit

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -14,6 +14,7 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return  SafeArea(
       child: Scaffold(
+
         resizeToAvoidBottomInset: false,
         body: ListView(
           children: <Widget> [
